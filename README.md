0a3b632d07a382d0c2593e5bdd45022cab313e07
Thomas Darby <tgd16@my.fsu.edu>
2018-12-12 08:06:50 +0000

    Extra Credit final commit

--- README.md
+++ README.md
@@ -4,22 +4,21 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+December 12, 2018
 
 ### Student Name:  
-(First and Last Name)
+Thomas Darby
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+This assignment involves connecting a web application to an existing API in order to receive and display information about the weather for the location the user inputs. 
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://tgdarby.github.io/WeatherChannel/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
-4. (Briefly describe a lesson/concept learned in this lesson.)
-5. (Briefly describe a lesson/concept learned in this lesson.)
+1. API keys can be used to track and control how an API is used.
+2. An API-centric web application executes most, if not all, of its functions through calling an API. 
+3. APIs can have a variety of query parameters that are sent as an HTTP Request to the API server.
+4. API methods are used to retrieve various types of data from the API server.
+5. Each HTML element in the DOM has an id associated with a "key" in the key/value pair from the data retrieved from the API server.
 
--- apixu_forecast.js
+++ apixu_forecast.js
@@ -1,8 +1,8 @@
 
 /*
     Program Name:   Seminole Weather Channel
-    Author: 
-    Date: 
+    Author:     Thomas Darby
+    Date:   December 12, 2018
     Filename:   apixu_forecast.js
 */
 
@@ -46,8 +46,6 @@
                                  
                      url: 'https://api.apixu.com/v1/forecast.json?key=ADD_YOUR_API_KEY_HERE',
                      
-
-        
                      
                      
                 IMPORTANT:  Before proceeding to Activity 2, save your Cloud9 workspace and preview this application 
@@ -89,10 +87,10 @@
 
         $(document).ready(function(){
            $(':button').click(function(){
-                var message = 'q=' + $('#searchValue').val()+'&days=3';   
+                var message = 'q=' + $('#searchValue').val()+'&days=7';   
                 $.ajax({
                       type: 'GET',
-                      url: 'https://api.apixu.com/v1/forecast.json?key=ADD_YOUR_API_KEY_HERE',
+                      url: 'https://api.apixu.com/v1/forecast.json?key=3c471fa6329244428a954850181212',
                       data: message             
                 })
                 .done(function(json){
@@ -104,8 +102,17 @@
                      $('#Day1High').html("<center>" + json.forecast.forecastday[0].day.maxtemp_f + "</center>");
                      $('#Day2Low').html("<center>" + json.forecast.forecastday[1].day.mintemp_f + "</center>");
                      $('#Day2High').html("<center>" + json.forecast.forecastday[1].day.maxtemp_f + "</center>");        
+                     $('#Day3Low').html("<center>" + json.forecast.forecastday[2].day.mintemp_f + "</center>");
+                     $('#Day3High').html("<center>" + json.forecast.forecastday[2].day.maxtemp_f + "</center>");
+                     $('#Day4Low').html("<center>" + json.forecast.forecastday[3].day.mintemp_f + "</center>");
+                     $('#Day4High').html("<center>" + json.forecast.forecastday[3].day.maxtemp_f + "</center>");
+                     $('#Day5Low').html("<center>" + json.forecast.forecastday[4].day.mintemp_f + "</center>");
+                     $('#Day5High').html("<center>" + json.forecast.forecastday[4].day.maxtemp_f + "</center>");
+                     $('#Day6Low').html("<center>" + json.forecast.forecastday[5].day.mintemp_f + "</center>");
+                     $('#Day6High').html("<center>" + json.forecast.forecastday[5].day.maxtemp_f + "</center>");
+                     $('#Day7Low').html("<center>" + json.forecast.forecastday[6].day.mintemp_f + "</center>");
+                     $('#Day7High').html("<center>" + json.forecast.forecastday[6].day.maxtemp_f + "</center>");
 
-                     
                 })
                 .fail(function() {
                       alert("Posting failed.");              
